f901eebaba10a7f62c06e8d0c54297f2a893535f
Erickkaranja <karanja99erick@gmail.com>
2023-06-25 19:34:54 +0300

    correction

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import String, Column
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
